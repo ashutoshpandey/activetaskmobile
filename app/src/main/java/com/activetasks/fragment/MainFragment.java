0d8dd5ebdb1d7926381441aeaeab40fc6d13a8f3
ashutosh <ashutosh.options4u@gmail.com>
2015-05-14 10:14:39 +0530

    ui updates, dashboard update

--- app/src/main/java/com/activetasks/fragment/MainFragment.java
+++ app/src/main/java/com/activetasks/fragment/MainFragment.java
@@ -4,18 +4,34 @@ package com.activetasks.fragment;
  * Created by ashutosh on 06/05/2015.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.activetasks.activetasks.R;
+import com.activetasks.activity.LoginActivity;
+import com.activetasks.helper.DateHelper;
+import com.activetasks.pojo.Task;
+import com.activetasks.util.Data;
+import com.activetasks.util.GroupReader;
+import com.activetasks.util.JsonReaderSupport;
+import com.activetasks.util.PendingTasksCountReader;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainFragment extends Fragment {
+
+    private TextView tvTasksRemaining;
+    private TextView tvGroupsCreated;
 
     public MainFragment() {
     }
@@ -24,6 +40,80 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        tvTasksRemaining = (TextView) rootView.findViewById(R.id.tvTasksRemaining);
+        tvGroupsCreated = (TextView) rootView.findViewById(R.id.tvGroupsCreated);
+
+        new RemainingTaskReadTask().execute();
+
+        new GroupCountReadTask().execute();
+
         return rootView;
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    class RemainingTaskReadTask implements JsonReaderSupport {
+
+        private String url = "http://10.0.2.2/activetask/data-pending-tasks-count/" + Data.userId;
+
+        public RemainingTaskReadTask() {
+            execute();
+        }
+
+        public void execute(){
+            PendingTasksCountReader reader = new PendingTasksCountReader(this, url);
+            reader.execute();
+        }
+
+        @Override
+        public void onJsonReadComplete(String result) {
+
+            try {
+                JSONObject json = new JSONObject(result);
+
+                String count = json.getString("count");
+
+                tvTasksRemaining.setText("Tasks remaining = " + count);
+            }
+            catch(Exception ex){
+                Log.d("Group ex", ex.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    class GroupCountReadTask implements JsonReaderSupport {
+
+        private String url = "http://10.0.2.2/activetask/data-all-groups-count/" + Data.userId;
+
+        public GroupCountReadTask() {
+            execute();
+        }
+
+        public void execute(){
+            PendingTasksCountReader reader = new PendingTasksCountReader(this, url);
+            reader.execute();
+        }
+
+        @Override
+        public void onJsonReadComplete(String result) {
+
+            try {
+                JSONObject json = new JSONObject(result);
+
+                String count = json.getString("count");
+
+                tvGroupsCreated.setText("Groups created = " + count);
+            }
+            catch(Exception ex){
+                Log.d("Group ex", ex.getMessage());
+            }
+        }
     }
 }
